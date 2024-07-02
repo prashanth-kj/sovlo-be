889f36d9e9250904ef042516fcf73be3ab9ea351
prashanth-kj <kjprasanth98@gmail.com>
2024-07-02 21:00:46 +0530

    updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT= process.env.PORT;
 const app= express();
   app.use(express.json());
   app.use(cors());
-  app.use(express.static("Public"))
+  app.use(express.static("./Public/images"))
   app.use('/', AppRoutes);
 
   app.listen(PORT, ()=>console.log(`App is listening PORT ${PORT}`))
