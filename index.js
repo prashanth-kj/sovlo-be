725a5e1d4d9ce6301dc027b3dfe2c7e3ebff62bb
prashanth-kj <kjprasanth98@gmail.com>
2024-07-03 19:59:40 +0530

    update

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ dotenv.config();
 const PORT= process.env.PORT;
 
 const app= express();
-  app.use(express.json());
   app.use(cors());
+  app.use(express.json());
   app.use(express.static('Public'))
+  
+ 
   app.use('/', AppRoutes);
 
   app.listen(PORT, ()=>console.log(`App is listening PORT ${PORT}`))
